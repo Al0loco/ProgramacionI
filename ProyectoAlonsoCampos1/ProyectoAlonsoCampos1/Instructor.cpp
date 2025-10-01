42787a11eb3eb169af1dd4e0bca90e73934956a8
Al0loco <alcam2004@gmail.com>
2025-09-30 22:44:14 -0600

    instructor

--- ProyectoAlonsoCampos1/ProyectoAlonsoCampos1/Instructor.cpp
+++ ProyectoAlonsoCampos1/ProyectoAlonsoCampos1/Instructor.cpp
@@ -8,41 +8,50 @@ Instructor::Instructor() {
 	fechaNacimiento = new string("01/01/2000");	
 	especialidades = nullptr; // inicializar el arreglo dinámico como nulo
 }
-Instructor::Instructor(string*, string*, string*, string*, string*) {
-
+// Constructor con parámetros
+Instructor::Instructor(string* cedula, string* nombre, string* telefono, string* correo, string* fechaNacimiento) {
+	this->cedula = cedula;
+	this->nombre = nombre;
+	this->telefono = telefono;
+	this->correo = correo;
+	this->fechaNacimiento = fechaNacimiento;
 }
 Instructor::~Instructor() {
-
+	delete cedula;
+	delete nombre;
+	delete telefono;
+	delete correo;
+	delete fechaNacimiento;
 }
 void Instructor::setCedula(string*) {
-
+	*cedula = *cedula;
 }
 void Instructor::setNombre(string*) {
-
+	*nombre = *nombre;
 }
 void Instructor::setTelefono(string*) {
-
+	*telefono = *telefono;
 }
 void Instructor::setCorreo(string*) {
-
+		*correo = *correo;
 }
 void Instructor::setFechaNacimiento(string*) {
-
+	*fechaNacimiento = *fechaNacimiento;
 }
 string* Instructor::getCedula() {
-
+	return cedula;
 }
 string* Instructor::getNombre() {
-
+		return nombre;
 }
 string* Instructor::getTelefono() {
-
+	return telefono;
 }
 string* Instructor::getCorreo() {
-
+		return correo;
 }
 string* Instructor::getFechaNacimiento() {
-
+		return fechaNacimiento;
 }
 void Instructor::addEspecialidad(string*) {
 
